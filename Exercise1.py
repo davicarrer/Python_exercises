8c1e9d30babeb30080512ff9a12016212c130c50
Andrea Davi Carrer <78238769+davicarrer@users.noreply.github.com>
2021-03-23 01:47:57 -0300

    Update Exercise1.py

--- Exercise1.py
+++ Exercise1.py
@@ -1,1 +1,14 @@
+#1 - Declare duas variáveis numéricas 'a' e 'b' e atribua os valores 7 e 12, respectivamente;
+#2 - Declare uma variável chamada 'resultado' e armazene o resultado da soma entre as duas variáveis declaradas anteriormente;
+#3 - Exiba o resultado da soma no console;
+#4 - Agora declare novas linhas de exibição no console e exiba o resultado da subtração, multiplicação, divisão, exponenciação e resto da divisão inteira
 
+a = 5
+b= 12
+resultado = a + b
+print(resultado)
+print("Subtração = ",a - b)
+print("Multiplicação = ", a * b)
+print("Divisão = ",a / b)
+print("Exponenciação", a ** b)
+print("Resto da divisão", a % b)
