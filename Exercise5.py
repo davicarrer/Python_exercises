e862f48e796766197a234aef3129313989892c9a
Andrea Davi Carrer <78238769+davicarrer@users.noreply.github.com>
2021-03-23 22:16:07 -0300

    Create Exercise5.py

---
+++ Exercise5.py
@@ -0,0 +1,7 @@
+#Resolução do Exercício 5 proposto pelo Prof Paulo Salvatore
+
+numero = int(input("Digite um numero inteiro positivo: "))
+if (numero % 2 == 0):
+    print(f"O numero {numero} e par.")
+else:
+    print(f"O numero {numero} e impar.")
